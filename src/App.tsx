af6253fa537ca6f324c8a3ce7851261e035d6710
bbeetlesam <samsismines@gmail.com>
2025-08-08 14:08:03 +0700

    re-structure the App elements (using main, footer, header, etc)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from "./components/Header.tsx";
+import Main from "./components/Main.tsx";
+import Footer from "./components/Footer.tsx";
+import RoughLine from "./components/RoughLine.tsx";
 import './App.css'
 
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="header">We're Only In It For The Money</h1>
-      <p className="read-the-docs">
-        what's the ugliest part of your body?
-      </p>
+      <Header />
+      <RoughLine />
+      <Main />
+      <RoughLine />
+      <Footer />
     </>
   )
 }
