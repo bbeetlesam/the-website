3fc6ff80ade949e5518f3e2de473521cfa26f2c2
bbeetlesam <samsismines@gmail.com>
2025-08-08 14:07:07 +0700

    add: header, main, and footer comp (separated for App)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+// footer component
+
+function Footer(){
+  return (
+    <footer className="w-screen">
+      <p>this one is a bloody footer.</p>
+      <p>&copy;bbeetlesam 2025. Made by a ret--ded individual.</p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+// header component
+
+function Header(){
+  return (
+    <header className="w-screen text-2xl">
+      <p>this is a fucking header</p>
+    </header>
+  )
+}
+
+export default Header;
