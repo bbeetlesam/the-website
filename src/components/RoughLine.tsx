c132053d674c9ae71a774ca3c9f7f832b0150920
bbeetlesam <samsismines@gmail.com>
2025-08-10 00:13:29 +0700

    makes RoughLine refresh each page resize/refresh

--- src/components/RoughLine.tsx
+++ src/components/RoughLine.tsx
@@ -6,24 +6,49 @@ function RoughLine() {
   const ref = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
-    if (ref.current) {
+    if (!ref.current) return;
+
+    const drawLine = () => {
+      if (!ref.current) return;
+
       const rc = rough.svg(ref.current);
-      const centerY = 6 / 2;
-      const node = rc.line(0, centerY, ref.current.clientWidth, centerY, {
-        stroke: "#272727",
-        strokeWidth: 3,
-        roughness: 1,
-      });
+      const centerY = 10 / 2;
+
+      const node = rc.line(
+        0,
+        centerY,
+        ref.current.clientWidth,
+        centerY,
+        {
+          stroke: "#272727",
+          strokeWidth: 3,
+          roughness: 8,
+          bowing: 0,
+          disableMultiStroke: false,
+          fillStyle: "zigzag",
+        }
+      );
+
       ref.current.innerHTML = "";
       ref.current.appendChild(node);
-    }
+    };
+
+    // init draw
+    drawLine();
+
+    // check resize
+    const resizeObserver = new ResizeObserver(drawLine);
+    resizeObserver.observe(ref.current);
+
+    // cleanup
+    return () => resizeObserver.disconnect();
   }, []);
 
   return (
     <svg
       ref={ref}
       width="100%"
-      height="6"
+      height="10"
       style={{ display: "block", margin: "0 auto" }}
       aria-hidden="true"
     />
