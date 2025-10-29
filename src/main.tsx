39f402acf5fae5f774b567f1431609a0b2e723a1
bbeetlesam <samsismines@gmail.com>
2025-10-29 09:26:04 +0700

    um, adding some easter texts on console (main.tsx)
    nothing more, nothing less.

--- src/main.tsx
+++ src/main.tsx
@@ -24,3 +24,18 @@ createRoot(document.getElementById('root')!).render(
     </Router>
   </StrictMode>,
 )
+
+const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+console.log(
+  '%c💿 bbeetlesam.dev 💿%c\n\n' +
+  'Hey you, out there on your own\n' +
+  'Sitting naked by the phone, would you touch me?\n\n' +
+  'Hey you, don\'t tell me there\'s no hope at all\n' +
+  'Together we stand, divided we fall\n',
+  isDarkMode
+    ? 'font-size: 20px; font-weight: bold; color: #1a1a1a; background: #f5f5f5;; padding: 8px 12px; border-radius: 10px;'
+    : 'font-size: 20px; font-weight: bold; color: #f5f5f5; background: #323237; padding: 8px 12px; border-radius: 10px;',
+  isDarkMode
+    ? 'font-size: 14px; color: #e0e0e0; padding: 3px;'
+    : 'font-size: 14px; color: #323237; padding: 3px;'  
+);
