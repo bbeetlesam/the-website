1a45e8c46674548b0955b56b0228b0d438d826c0
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-12-27 20:43:50 +0700

    refactor: change some texts (inc. footer's tagline) in text-contents.ts
    while also reduce the total lines by squashing the codes into more
    unreadable, good-looking structure

--- src/data/text-contents.ts
+++ src/data/text-contents.ts
@@ -1,55 +1,29 @@
+import type { RouteItem, SocialLink } from "../types/common";
+
 // navigation routes
-const NAV_ROUTES = [
-  {
-    path: "/games",
-    label: "Games",
-    title: "Explore my games!",
-  },
-  {
-    path: "/oddworks",
-    label: "Oddworks",
-    title: "My other projects!",
-  },
-  {
-    path: "/arts",
-    label: "Arts",
-    title: "Pieces of my arts!",
-  },
-  {
-    path: "/blogs",
-    label: "Blogs",
-    title: "Read my thoughts!",
-  },
-  {
-    path: "/bio",
-    label: "Bio",
-    title: "Who am I really?",
-  },
+const NAV_ROUTES: RouteItem[] = [
+  { path: "/games", label: "Games", title: "Strange, arcade oddities." },
+  { path: "/oddworks", label: "Oddworks", title: "Other strange projects." },
+  { path: "/arts", label: "Arts", title: "Pieces of my arts." },
+  { path: "/blogs", label: "Blogs", title: "The thoughts of my mind." },
+  { path: "/bio", label: "Bio", title: "Who am I really?" },
 ];
 
 // social links
-const SOCIAL_LINKS = [
-  {
-    href: "https://bbeetlesam.itch.io",
-    title: "My weird games.",
-    icon: "fa-brands fa-itch-io",
-  },
-  {
-    href: "https://github.com/bbeetlesam",
-    title: "My chaos GitHub.",
-    icon: "fa-brands fa-github",
-  },
-  {
-    href: "https://instagram.com/jstsams",
-    title: "My unpopular Instagram.",
-    icon: "fa-brands fa-instagram",
-  },
+const SOCIAL_LINKS: SocialLink[] = [
+  { href: "mailto:bbeetlesam1968@gmail.com", title: "Email me (what for?).", icon: "fa-solid fa-envelope" },
+  { href: "https://github.com/bbeetlesam", title: "My git GitHub.", icon: "fa-brands fa-github", featuredInHeader: true },
+  { href: "https://bbeetlesam.itch.io", title: "My weird games.", icon: "fa-brands fa-itch-io", featuredInHeader: true },
+  { href: "https://www.linkedin.com/in/samudra-azriel-pradana-b48491321", title: "Looking for a job.", icon: "fa-brands fa-linkedin" },
+  { href: "https://instagram.com/jstsams", title: "My boring Instagram.", icon: "fa-brands fa-instagram", featuredInHeader: true },
+  { href: "https://twitter.com/bbeetlesam", title: "My quiet Twitter.", icon: "fa-brands fa-x-twitter" },
+  { href: "https://bbeetlesam.bsky.social", title: "My blue Bluesky.", icon: "fa-brands fa-bluesky" },
 ];
 
 // footer texts
 const FOOTER_TEXT = {
   copyright: "bbeetlesam 2025.",
-  tagline: "Keep in weird time and stay progressive.",
+  tagline: "The crux of the biscuit is the creator.",
 };
 
 export { NAV_ROUTES, SOCIAL_LINKS, FOOTER_TEXT };
