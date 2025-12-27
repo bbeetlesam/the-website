32c959b2867176c0efad34c4306bcbd2a40b2108
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-12-27 19:26:31 +0700

    added type/common.ts for centralised type def

---
+++ src/types/common.ts
@@ -0,0 +1,13 @@
+type RouteItem = {
+  path: string;
+  label: string;
+  title?: string;
+};
+
+type SocialLink = {
+  href: string;
+  title?: string;
+  icon: string;
+};
+
+export type { RouteItem, SocialLink };
