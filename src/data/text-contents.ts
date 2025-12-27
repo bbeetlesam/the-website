27ec8b64f2f8a933a10e7edbdf7bf6259062b4a2
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-12-27 18:49:39 +0700

    added text-contents.ts for centralized content handling

---
+++ src/data/text-contents.ts
@@ -0,0 +1,55 @@
+// navigation routes
+const NAV_ROUTES = [
+  {
+    path: "/games",
+    label: "Games",
+    title: "Explore my games!",
+  },
+  {
+    path: "/oddworks",
+    label: "Oddworks",
+    title: "My other projects!",
+  },
+  {
+    path: "/arts",
+    label: "Arts",
+    title: "Pieces of my arts!",
+  },
+  {
+    path: "/blogs",
+    label: "Blogs",
+    title: "Read my thoughts!",
+  },
+  {
+    path: "/bio",
+    label: "Bio",
+    title: "Who am I really?",
+  },
+];
+
+// social links
+const SOCIAL_LINKS = [
+  {
+    href: "https://bbeetlesam.itch.io",
+    title: "My weird games.",
+    icon: "fa-brands fa-itch-io",
+  },
+  {
+    href: "https://github.com/bbeetlesam",
+    title: "My chaos GitHub.",
+    icon: "fa-brands fa-github",
+  },
+  {
+    href: "https://instagram.com/jstsams",
+    title: "My unpopular Instagram.",
+    icon: "fa-brands fa-instagram",
+  },
+];
+
+// footer texts
+const FOOTER_TEXT = {
+  copyright: "bbeetlesam 2025.",
+  tagline: "Keep in weird time and stay progressive.",
+};
+
+export { NAV_ROUTES, SOCIAL_LINKS, FOOTER_TEXT };
