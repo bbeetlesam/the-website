c67090e87037c6cdb337534ba70b4c5099cc6316
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-12-29 00:54:30 +0700

    refactor: use NAV_ROUTES and simplify the code for desk-items.ts
    some new syntax here, but still comprehensible

--- src/data/desk-items.ts
+++ src/data/desk-items.ts
@@ -1,14 +1,13 @@
 // used for Home's desk items
+import { NAV_ROUTES } from "./text-contents";
+import type { RouteItem } from "../types/common";
 import placeholderImg from "../assets/gamepad.png";
 // import oddworksImg from "../assets/oddworks.png";
 // import artsImg from "../assets/arts.png";
 // import blogsImg from "../assets/blogs.png";
 // import bioImg from "../assets/bio.png";
 
-export type DeskItem = {
-  path: string;
-  label: string;
-  title?: string;
+export type DeskItem = RouteItem & {
   image: string;
   position: { // in percentages
     top: number;
@@ -18,50 +17,16 @@ export type DeskItem = {
   size: number; // in pixels
 };
 
-export const deskItems: DeskItem[] = [
-  {
-    path: "/games",
-    label: "Games",
-    title: "Explore my games!",
-    image: placeholderImg,
-    position: { top: 20, left: 20 },
-    rotation: 0,
-    size: 150,
-  },
-  {
-    path: "/oddworks",
-    label: "Oddworks",
-    title: "My other projects!",
-    image: placeholderImg,
-    position: { top: 60, left: 25 },
-    rotation: 12,
-    size: 100,
-  },
-  {
-    path: "/arts",
-    label: "Arts",
-    title: "Pieces of my arts!",
-    image: placeholderImg,
-    position: { top: 35, left: 50 },
-    rotation: -5,
-    size: 110,
-  },
-  {
-    path: "/blogs",
-    label: "Blogs",
-    title: "Read my thoughts!",
-    image: placeholderImg,
-    position: { top: 70, left: 70 },
-    rotation: 15,
-    size: 95,
-  },
-  {
-    path: "/bio",
-    label: "Bio",
-    title: "Who am I really?",
-    image: placeholderImg,
-    position: { top: 25, left: 82 },
-    rotation: -10,
-    size: 105,
-  },
+type DeskItemsConfig = Omit<DeskItem, keyof RouteItem>;
+
+const deskItemsConfigs: DeskItemsConfig[] = [
+  { image: placeholderImg, position: { top: 20, left: 20 }, rotation: 0, size: 150 },
+  { image: placeholderImg, position: { top: 60, left: 25 }, rotation: 12, size: 100 },
+  { image: placeholderImg, position: { top: 35, left: 50 }, rotation: -5, size: 110 },
+  { image: placeholderImg, position: { top: 70, left: 70 }, rotation: 15, size: 95 },
+  { image: placeholderImg, position: { top: 25, left: 82 }, rotation: -10, size: 105 },
 ];
+
+export const deskItems: DeskItem[] = NAV_ROUTES.map((route, index) => ({
+  ...route, ...deskItemsConfigs[index], // merge
+}));
