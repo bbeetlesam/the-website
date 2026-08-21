4f0777ceb1db2f22d2b17070491bfcb0335b2cbc
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-21 18:02:17 +0700

    feat: added Footer component (just a simple centred message)

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { FOOTER_TEXT } from '$lib/data';
+</script>
+
+<footer class="flex w-full items-center justify-center p-2 text-center text-xs">
+	<p>{FOOTER_TEXT}</p>
+</footer>
