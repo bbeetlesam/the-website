0972ae61f472d554bad4474fda6f7d9620b81a22
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-21 18:18:48 +0700

    thicken footer's text font from base (400) to medium (500)

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,6 +2,6 @@
 	import { FOOTER_TEXT } from '$lib/data';
 </script>
 
-<footer class="flex w-full items-center justify-center p-2 text-center text-xs">
+<footer class="flex w-full items-center justify-center p-2 text-center text-xs font-medium">
 	<p>{FOOTER_TEXT}</p>
 </footer>
