8d5622b5084dcd2a772f6adf52d1a16d7924e31b
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-21 18:17:44 +0700

    set the base layout for main (root) +page
    set it to the centre  of the root div's +layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,18 @@
 	import pic from '$lib/assets/favicon.svg';
 </script>
 
-<div class="flex h-screen w-screen items-center justify-center gap-2">
-	<img src={pic} class="h-30 w-auto" alt="favicon" />
-	<div class="space-y-1">
-		<h1 class="text-2xl font-medium">Welcome to Hell.</h1>
-		<p class="text-base">
-			Visit <a class="underline" href="https://github.com/bbeetlesam" target="_blank">something</a> to
-			read what?
-		</p>
+<!-- Homepage, landing page, viewport-fixed -->
+<div class="flex w-screen flex-1 items-center justify-center">
+	<div class="flex items-center gap-2">
+		<img src={pic} class="h-30 w-auto" alt="favicon" />
+
+		<div class="space-y-1">
+			<h1 class="text-2xl font-medium">Welcome to Hell.</h1>
+			<p class="text-base">
+				Visit
+				<a class="underline" href="https://github.com/bbeetlesam" target="_blank"> something </a>
+				to read what?
+			</p>
+		</div>
 	</div>
 </div>
