79bcd1e77543778b8d9378c2b3a670301e08b30c
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-23 05:16:59 +0700

    changed pickRandomItem to a 'real' function rather than arrow func
    also added some docs for clarity, i feel like i may be neurodegenerating

--- src/lib/utils/random.ts
+++ src/lib/utils/random.ts
@@ -1,6 +1,13 @@
-const pickRandomItem = <T>(items: T[]): T | undefined => {
+// These are utils that related to randomness
+// I feel random.
+
+/**
+ * Pick a random item from an array.
+ *
+ * @param items - array of items
+ * @returns random item from the array, or undefined if the array is empty
+ */
+export function pickRandomItem<T>(items: T[]): T | undefined {
 	if (items.length === 0) return undefined;
 	return items[Math.floor(Math.random() * items.length)];
-};
-
-export { pickRandomItem };
+}
