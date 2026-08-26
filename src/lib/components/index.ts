879ab51dcc4be15c0dbd06afff4638e9d4176615
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-08-26 14:30:29 +0700

    refactor: put Footer into components/site and not exposing it to components public api (in barrel file)
    because site components are rarely RE-used

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,5 +1,3 @@
-export { default as Footer } from './Footer.svelte';
-
 // Desk components
 export { default as DeskArea } from './desk/DeskArea.svelte';
 export { default as DeskItem } from './desk/DeskItem.svelte';
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import { Footer } from '$lib/components';
+	import { Footer } from '$lib/components/site';
 
 	let { children } = $props();
 </script>
