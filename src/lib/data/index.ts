5d54db68b0bfa410f0ee19813a625bcf66d09431
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2026-09-12 22:19:24 +0700

    feat(bio): fetch social links from Sanity for Bio page
    also added some props in SocialLink typeand remove data/socials.ts

--- src/lib/data/index.ts
+++ src/lib/data/index.ts
@@ -1,3 +1,2 @@
-export * from './socials';
 export * from './footer';
 export * from './navigations';
--- src/lib/types/social.ts
+++ src/lib/types/social.ts
@@ -2,12 +2,16 @@
  * Represents a social link.
  */
 type SocialLink = {
+	/** The unique identifier for the link. */
+	id: string;
 	/** The href of the link (to somewhere else). */
 	href: string;
 	/** The representing title for the link. */
 	title: string;
 	/** The image icon of the link. */
 	icon: string;
+	/** The order of the link in the list of social links. */
+	order: number;
 };
 
 export type { SocialLink };
